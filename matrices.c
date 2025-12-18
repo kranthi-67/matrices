64f41f3478a9612d3edc522f49372fb0a82bed54
kranthi-67 <kranthi09131@gmail.com>
2025-12-18 20:58:54 +0530

    Add files via upload
    simple 2x2 matrix

---
+++ matrices.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2] = {{10, 20}, {30, 40}};
+
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
